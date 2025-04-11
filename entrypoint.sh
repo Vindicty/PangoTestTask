1a6d62719f757345c02f5e49184363a2367d3d2f
Vindic <tilowolf@inbox.ru>
2025-04-11 11:09:41 +0300

    ci cd configuring

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 set -e
 
 echo "➡️  Запуск эмулятора..."
-nohup emulator -avd test -no-audio -no-window -gpu swiftshader_indirect -no-boot-anim &
+nohup emulator -avd test -no-audio -no-window -gpu swiftshader_indirect -no-boot-anim -no-snapshot -no-metrics > /app/emulator.log 2>&1 &
 
 echo "⏳ Ожидание запуска эмулятора..."
 adb wait-for-device
@@ -14,15 +14,18 @@ done
 echo "✅ Эмулятор загружен"
 
 echo "🚀 Запуск Appium..."
-nohup appium --base-path /wd/hub > /app/appium.log 2>&1 &
+nohup appium --address 0.0.0.0 --port 4723 --base-path /wd/hub > /app/appium.log 2>&1 &
 
 echo "⏳ Ожидание старта Appium..."
-until curl --output /dev/null --silent --head --fail http://127.0.0.1:4723/wd/hub; do
+until curl --silent http://127.0.0.1:4723/wd/hub/status || curl --silent http://0.0.0.0:4723/wd/hub/status; do
     sleep 1
 done
 echo "✅ Appium работает"
 
+echo "📄 Последние 20 строк лога Appium:"
+tail -n 20 /app/appium.log || true
+
 echo "🧪 Запуск тестов..."
-pytest automation_framework/tests --html=automation_framework/tests/logs/report.html
+pytest automation_framework/tests --html=automation_framework/tests/logs/report.html || true
 
 exec "$@"
